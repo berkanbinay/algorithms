5b344055f6a79c4e339b35621e3c4ef030032f3b
bekranbinay <binayberkan@gmail.com>
2022-07-10 21:12:08 +0300

    .

---
+++ sorting-algorithms/selection-sort/src/sorting_algorithms/SelectionSort.java
@@ -0,0 +1,36 @@
+package sorting_algorithms;
+
+public class SelectionSort {
+
+    static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for(int i = 0; i < n -1; i ++) {
+            int index_min = i;
+            for (int j = i; j < n; j++) {
+                if(arr[j] < arr[index_min]) {
+                    index_min = j;
+                }
+            }
+            if(index_min != i) {
+                int temp = arr[i];
+                arr[i] = arr[index_min];
+                arr[index_min] = temp;
+            }
+        }
+    }
+    static void print(int arr[]) {
+        for(int item : arr){
+            System.out.print(item + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {16,9, 23, 7, 55, 44};
+        System.out.print("before sorting -> ");
+        print(arr);
+        System.out.print("\nafter sorting -> ");
+        selectionSort(arr);
+        print(arr);
+
+    }
+}
